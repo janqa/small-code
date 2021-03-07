6e9c0ad89168d6c27e9d7a92bf845ec3dcd8e0fb
janqa <jan.bronnikau@gmail.com>
2021-03-07 14:27:06 +0300

    Initial commit

---
+++ src/main/java/example/smallcode/Main.java
@@ -0,0 +1,29 @@
+package example.smallcode;
+
+import java.math.BigInteger;
+
+public class Main {
+    private static final BigInteger THREE = BigInteger.valueOf(3);
+    private static final BigInteger ONE = BigInteger.ONE;
+    private static final BigInteger TWO = BigInteger.TWO;
+
+    public static void main(String... args) {
+
+    }
+
+    public static Object solution(BigInteger n) {
+        var num = n;
+        var moves = 0;
+        while (num.equals(ONE)) {
+            if (num.mod(TWO).equals(0))
+                num = num.divide(TWO);
+            else if (num.equals(THREE))
+                num = num.min(ONE);
+            else if (num.toString().substring(num.bitCount() - 2).equals("11")) {
+            } else
+                num = num.min(ONE);
+            moves += 1;
+        }
+        return moves;
+    }
+}
